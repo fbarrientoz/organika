c99fa96708bc210de3a56fa260313efa3f8e7fbd
fbarrientos <fbarrientos@uabc.edu.mx>
2020-10-22 01:03:40 -0500

    Commit for homework1

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';//imrc
+import "./footer.css";
+class Footer extends Component { //cc
+    render() { 
+        return (     
+            <React.Fragment>
+                <div className="test">Footer will be here</div>
+                <div className="test">Fabiolaa</div>
+            </React.Fragment>        
+            
+         );
+    }
+}
+ 
+export default Footer;
