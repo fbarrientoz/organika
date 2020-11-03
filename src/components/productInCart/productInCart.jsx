b0f7fdf4cb3ef92a3a09d0934bbe24891227e9c5
fbarrientos <fbarrientos@uabc.edu.mx>
2020-11-03 00:57:13 -0600

    - The user can add items to cart (and that will be saved into redux store)
    - The user can see the list of items on the cart by visiting cart page.
    
    - The user can remove products from the cart by pressing a button.

--- src/components/productInCart/productInCart.jsx
+++ src/components/productInCart/productInCart.jsx
@@ -8,17 +8,17 @@ class ProductinCart extends Component {
     render(){
         return (
             <React.Fragment>
-                <div id="cartDisplay">
-                    <div id="imgDisplay">
+                <div id="cart-display">
+                    <div id="img-display">
                         <img src={"/images/products/" + this.props.data.product.image} id="cartImg" alt="" width="200px" height="200px"/>
                     </div>
-                    <div id="cartMain">
+                    <div id="cart-main">
                         <h1><b>{this.props.data.product.title}</b></h1>
-                        <h2><b>Quantity:</b>{this.props.data.quantity}</h2>
+                        <p><b>Quantity:</b>{this.props.data.quantity}</p>
                         <h5><b>Unit Price:</b>${this.props.data.product.price}</h5>
                     </div>
-                    <div id="cartTotal">
-                        <h5 id="displayTotal"><b>Item Total:</b> ${this.props.data.product.price * this.props.data.quantity}</h5>
+                    <div id="cart-total">
+                        {/* <h5 id="displayTotal"><b>Item Total:</b> ${this.props.data.product.price * this.props.data.quantity}</h5> */}
                         <button onClick={this.removeClicked} className="btn btn-warning">Delete from Cart</button>
                     </div>
                 </div>
